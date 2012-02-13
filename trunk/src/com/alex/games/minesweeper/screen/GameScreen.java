8e1e2d8c94e6c75e3d82118272238b4c6f890aa0
alexlloreda@gmail.com <alexlloreda@gmail.com@dc73df27-50b8-9c5a-1dad-c84df949fc2d>
2012-02-13 07:40:46 +0000

    First commit

---
+++ trunk/src/com/alex/games/minesweeper/screen/GameScreen.java
@@ -0,0 +1,49 @@
+package com.alex.games.minesweeper.screen;
+
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen {
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(float deltaTime) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resize(int w, int h) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
